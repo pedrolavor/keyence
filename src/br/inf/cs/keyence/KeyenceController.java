8791167b1e2e89160a12aec9ae5c083354432714
Pedro Lavôr <thiagolavor33@gmail.com>
2018-10-16 18:18:55 -0400

    Melhora dos metodos

--- src/br/inf/cs/keyence/KeyenceController.java
+++ src/br/inf/cs/keyence/KeyenceController.java
@@ -1,13 +1,15 @@
 package br.inf.cs.keyence;
 
+import java.io.InputStream;
+
 public class KeyenceController {
 	
-	Keyence scanner;
+	Keyence scanner = null;
 	
 	public KeyenceController() {
 		this.scanner = new Keyence();
 	}
-
+	
 	public void conectar() throws Exception {
 		this.scanner.connect();
 	}
@@ -16,28 +18,82 @@ public class KeyenceController {
 		this.scanner.close();
 	}
 	
-	public void iniciar() {
-		
+	public String scanear(int bank) {
+		String resposta = null;
+		try {
+			resposta = this.scanner.processCommand("LON," + (bank < 10 ? "0" : "") + bank);
+		} catch (Exception e) {
+			System.out.println("Erro ao tentar scanear: " + e.getMessage());
+		}
+		return resposta;
 	}
 	
-	public void parar() {
-		
+	public String scanear() {
+		return scanear(1);
 	}
 	
-	public void comando(String command) throws Exception {
-		this.scanner.processCommand(command);
+	public String focar() {
+		String resposta = null;
+		try {
+			resposta = this.scanner.processCommand("FTUNE");
+		} catch (Exception e) {
+			System.out.println("Erro ao tentar focar: " + e.getMessage());
+		}
+		return resposta;
 	}
 	
-	public void testar() throws Exception {
-		if(this.scanner.isConnected()) {
-			this.comando("TEST1");
-			Thread.sleep(3000);
-			this.comando("QUIT");
-			System.out.println("Teste realizado com sucesso!");
-		} else {
-			throw new Exception("O scanner não está conectado!");
+	public String tunar(int bank) {
+		String resposta = null;
+		try {
+			resposta = this.scanner.processCommand("TUNE," + (bank < 10 ? "0" : "") + bank);
+		} catch (Exception e) {
+			System.out.println("Erro ao tentar tunar: " + e.getMessage());
+		}
+		return resposta;
+	}
+	
+	public String tunar() {
+		return tunar(1);
+	}
+	
+	public String pararTunagem() {
+		String resposta = null;
+		try {
+			resposta = this.scanner.processCommand("TQUIT");
+		} catch (Exception e) {
+			System.out.println("Erro ao tentar scanear: " + e.getMessage());
+		}
+		return resposta;
+	}
+	
+	public String pararLeitura() {
+		String resposta = null;
+		try {
+			resposta = this.scanner.processCommand("LOFF");
+		} catch (Exception e) {
+			System.out.println("Erro ao tentar parar leitura: " + e.getMessage());
+		}
+		return resposta;
+	}
+	
+	public InputStream stream() {
+		try {
+			return this.scanner.listenStream();
+		} catch (Exception e) {
+			return null;
 		}
 	}
+	
+	
+	
+	
+	
+	
+
+	
+	public boolean conectado() {
+		return this.scanner.isConnected();
+	}	
 	
 	public void nome(String name) {
 		this.scanner.setName(name);
